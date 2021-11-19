975ed397d3a3bec08a3a1f713c275ab99077cdbf
Brian Moriguchi <brian@beckos.com>
2021-11-18 17:40:53 -0800

    This is actually my second SwiftUI sample app.

--- MyFirstSwiftUIApp/ContentView.swift
+++ MyFirstSwiftUIApp/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+        List(0..<10) { _ in
+            Text("Hello, world!")
+                .foregroundColor(.orange)
+                .padding()
+                .font(.system(size: 20, weight: .bold, design: .default))
+                
+            
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
     }
 }
