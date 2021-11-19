13003971e0d2662d00aee1bcd5fccd8858538d61
Brian Moriguchi <brian@beckos.com>
2021-11-18 16:28:18 -0800

    Initial Commit

---
+++ MyFirstSwiftUIApp/MyFirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIAppApp.swift
+//  MyFirstSwiftUIApp
+//
+//  Created by Tatsuya Moriguchi on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
